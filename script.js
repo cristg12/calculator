60a82c159cd3adca5919a9fbc423ff41790489b2
Cristian <cristiantrenado41@gmail.com>
2024-08-13 14:24:54 +0200

    setting up html & css

---
+++ script.js
@@ -0,0 +1,21 @@
+ 
+ const num1 = ""
+
+ const num2= ""
+
+ const operate = ""
+
+ function add (a, b){
+    return a + b;
+ }
+
+ function subtract (a, b){
+    return a - b;
+ }
+
+ function multiply (){
+    return a * b;
+ }
+ function divide(){
+    return a / b;
+ }
